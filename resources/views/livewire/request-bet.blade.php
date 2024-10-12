87bfcb27705edeaebbf7a6dbf9b5cd18b7763276
blesskpontsu <blesskpontsu@gmail.com>
2024-10-12 18:30:51 +0000

    updated request bet

--- resources/views/livewire/request-bet.blade.php
+++ resources/views/livewire/request-bet.blade.php
@@ -134,11 +134,11 @@
         const hours = currentTime.getHours();
 
         if (this.company === 'National Lottery Authority') {
-            if (hours < 9) {
+            if (hours < 10) {
                 this.game_times = ['morning', 'afternoon', 'evening'];
-            } else if (hours < 12) {
+            } else if (hours < 13) {
                 this.game_times = ['afternoon', 'evening'];
-            } else if (hours < 18) {
+            } else if (hours < 19) {
                 this.game_times = ['evening'];
             }
         } else if (this.company === 'Afriluck NLA') {
