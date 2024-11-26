90a22d4e51ce83cc322e7da1aa8054341e50ba5e
blesskpontsu <blesskpontsu@gmail.com>
2024-11-26 13:45:39 +0000

    fixes

--- app/Livewire/Auth/Subscribe.php
+++ app/Livewire/Auth/Subscribe.php
@@ -140,7 +140,7 @@ class Subscribe extends Component
             'description' => 'Subscription for Iyke63',
             'startTime' => Carbon::now()->addMinutes(5)->toTimeString('minute'),
             'paymentInterval' => $paymentInterval,
-            'customerMobileNumber' => '233241747683',
+            'customerMobileNumber' => '233543531563',
             'paymentOption' => 'MobileMoney',
             'Channel' => 'mtn_gh_rec',
             'recurringAmount' => 1.00,
