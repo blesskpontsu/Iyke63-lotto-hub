b3d71f3b8fd5126fb74d2e9c9ab8967670cf08e7
blesskpontsu <blesskpontsu@gmail.com>
2024-12-20 21:38:00 +0000

    fixes

--- resources/views/livewire/auth/subscribe.blade.php
+++ resources/views/livewire/auth/subscribe.blade.php
@@ -6,7 +6,7 @@
             <p class="text-center text-xl">Pay with Mobile Money (GHANAIANS ONLY)</p>
         </div>
         <div>
-            <button wire:click="cardSubscription"><img class="rounded-xl mx-auto" src="{{ asset('images/credit.png') }}" alt=""></button>
+            <button wire:click="hubtel_card_subscription"><img class="rounded-xl mx-auto" src="{{ asset('images/credit.png') }}" alt=""></button>
             <p class="text-center text-xl">Pay with credit card</p>
         </div>
     </div>
