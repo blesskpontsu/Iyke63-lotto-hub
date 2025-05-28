509ed3cca8c7bccb0ab62b82d10fee09a34fb805
blessafriluck <bless@afriluck.com>
2025-05-28 13:49:14 +0000

    fixes

--- config/webhook-client.php
+++ config/webhook-client.php
@@ -13,7 +13,7 @@ return [
              * We expect that every webhook call will be signed using a secret. This secret
              * is used to verify that the payload has not been tampered with.
              */
-            'signing_secret' => env('PAYSTACK_SECRET'),
+            'signing_secret' => config('services.paystack.live_key'),
 
             /*
              * The name of the header containing the signature.
